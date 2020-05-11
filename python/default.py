f5f0a2c4d06b1a65636255ef3402dd669d2b1821
fckorea <fckorea7@gmail.com>
2020-05-11 18:29:48 +0900

    python: change indent

--- python/default.py
+++ python/default.py
@@ -27,110 +27,109 @@ CONFIG = {}
 
 #=============================== Main Functions ===============================#
 def fnMain(argOptions, argArgs):
-    global LOGGER
+  global LOGGER
 
-    try:
-        return True
-        
-    except:
-        LOGGER.error(' *** Error in Main.')
-        LOGGER.debug(traceback.format_exc())
-    finally:
-        return True
+  try:
+    return True
+  except:
+    LOGGER.error(' *** Error in Main.')
+    LOGGER.debug(traceback.format_exc())
+  finally:
+    return True
 
 #=============================== Config & Init Function ===============================#
 def fnGetConfig(argConfigFilePath):
-    global LOGGER
-    global CONFIG
+  global LOGGER
+  global CONFIG
 
-    try:
-        if os.path.isfile(argConfigFilePath):
-            CONFIG = json.loads(open(argConfigFilePath, encoding='UTF8').read())
-            LOGGER.info(' * Read config data')
-            return True
-        else:
-            LOGGER.error(' * Config file not found.')
-    except:
-        LOGGER.error(' *** Error read config file.')
-        LOGGER.debug(traceback.format_exc())
-    
-    return False
+  try:
+    if os.path.isfile(argConfigFilePath):
+      CONFIG = json.loads(open(argConfigFilePath, encoding='UTF8').read())
+      LOGGER.info(' * Read config data')
+      return True
+    else:
+      LOGGER.error(' * Config file not found.')
+  except:
+    LOGGER.error(' *** Error read config file.')
+    LOGGER.debug(traceback.format_exc())
+  
+  return False
 
 def fnInit(argOptions):
-    global PROG_NAME
-    global LOGGER
-    global LOG_DIR
-    global LOG_FILENAME
+  global PROG_NAME
+  global LOGGER
+  global LOG_DIR
+  global LOG_FILENAME
 
-    if os.path.isdir(os.path.abspath(LOG_DIR)) is False:
-        os.mkdir(os.path.abspath(LOG_DIR))
+  if os.path.isdir(os.path.abspath(LOG_DIR)) is False:
+    os.mkdir(os.path.abspath(LOG_DIR))
 
-    LOGGER = logging.getLogger(PROG_NAME.replace(' ', ''))
+  LOGGER = logging.getLogger(PROG_NAME.replace(' ', ''))
 
-    if argOptions.o_bVerbose is True:
-        LOGGER.setLevel(logging.DEBUG)
-    else:
-        LOGGER.setLevel(logging.INFO)
+  if argOptions.o_bVerbose is True:
+    LOGGER.setLevel(logging.DEBUG)
+  else:
+    LOGGER.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('[%(levelname)s] - %(filename)s:%(lineno)s\t- %(asctime)s - %(message)s')
-    
-    file_handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when='midnight', backupCount=7, encoding='UTF-8')
-    file_handler.suffix = '%Y%m%d'
-    file_handler.setFormatter(formatter)
+  formatter = logging.Formatter('[%(levelname)s] - %(filename)s:%(lineno)s\t- %(asctime)s - %(message)s')
+  
+  file_handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when='midnight', backupCount=7, encoding='UTF-8')
+  file_handler.suffix = '%Y%m%d'
+  file_handler.setFormatter(formatter)
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+  stream_handler = logging.StreamHandler()
+  stream_handler.setFormatter(formatter)
 
-    LOGGER.addHandler(file_handler)
-    LOGGER.addHandler(stream_handler)
+  LOGGER.addHandler(file_handler)
+  LOGGER.addHandler(stream_handler)
 
-    return True
+  return True
 
 #=============================== OptionParser Functions ===============================#
 def fnSetOptions():
-    global PROG_VER
+  global PROG_VER
 
-    parser = None
+  parser = None
 
-    # Ref. https://docs.python.org/2/library/optparse.html#optparse-reference-guide
-    options = [
-        { 'Param': ('-c', '--config'), 'action': 'store', 'type': 'string', 'dest': 'o_sConfigFilePath', 'default': 'config.conf', 'metavar': '<Config file path>', 'help': 'Set config file path.\t\tdefault) config.conf (contents type is JSON)' },
-        { 'Param': ('-v', '--verbose'), 'action': 'store_true', 'dest': 'o_bVerbose', 'default': False, 'metavar': '<Verbose Mode>', 'help': 'Set verbose mode.\t\tdefault) False' },
-        { 'Param': ('-t', '--true'), 'action': 'store_true', 'dest': 'o_bTrue', 'default': False, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) False' },
-        { 'Param': ('-f', '--false'), 'action': 'store_false', 'dest': 'o_bFalse', 'default': True, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) True' },
-        { 'Param': ('-s', '--string'), 'action': 'store', 'type': 'string', 'dest': 'o_sString', 'metavar': '<String>', 'help': 'Set string.' },
-        { 'Param': ('-i', '--int'), 'action': 'store', 'type': 'int', 'dest': 'o_iInt', 'metavar': '<Int>', 'help': 'Set int.' },
-    ]
-    usage = '%prog [options] <File or Dir path>\n\tex) %prog test\\'
+  # Ref. https://docs.python.org/2/library/optparse.html#optparse-reference-guide
+  options = [
+    { 'Param': ('-c', '--config'), 'action': 'store', 'type': 'string', 'dest': 'o_sConfigFilePath', 'default': 'config.conf', 'metavar': '<Config file path>', 'help': 'Set config file path.\t\tdefault) config.conf (contents type is JSON)' },
+    { 'Param': ('-v', '--verbose'), 'action': 'store_true', 'dest': 'o_bVerbose', 'default': False, 'metavar': '<Verbose Mode>', 'help': 'Set verbose mode.\t\tdefault) False' },
+    { 'Param': ('-t', '--true'), 'action': 'store_true', 'dest': 'o_bTrue', 'default': False, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) False' },
+    { 'Param': ('-f', '--false'), 'action': 'store_false', 'dest': 'o_bFalse', 'default': True, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) True' },
+    { 'Param': ('-s', '--string'), 'action': 'store', 'type': 'string', 'dest': 'o_sString', 'metavar': '<String>', 'help': 'Set string.' },
+    { 'Param': ('-i', '--int'), 'action': 'store', 'type': 'int', 'dest': 'o_iInt', 'metavar': '<Int>', 'help': 'Set int.' },
+  ]
+  usage = '%prog [options] <File or Dir path>\n\tex) %prog test\\'
 
-    parser = OptionParser(usage = usage, version = '%prog ' + PROG_VER)
+  parser = OptionParser(usage = usage, version = '%prog ' + PROG_VER)
 
-    for option in options:
-        param = option['Param']
-        del option['Param']
-        parser.add_option(*param, **option)
+  for option in options:
+    param = option['Param']
+    del option['Param']
+    parser.add_option(*param, **option)
 
-    return parser
+  return parser
 
 def fnGetOptions(argParser):
-    if len(sys.argv) == 1:
-        return argParser.parse_args(['--help'])
+  if len(sys.argv) == 1:
+    return argParser.parse_args(['--help'])
 
-    if len(argParser.parse_args()[1]) == 0:
-        return argParser.parse_args(['--help'])
+  if len(argParser.parse_args()[1]) == 0:
+    return argParser.parse_args(['--help'])
 
-    return argParser.parse_args()
+  return argParser.parse_args()
 
 if __name__ == '__main__':
-    ### REMOVE
-    if PROG_NAME == '<PROGRAM_NAME>':
-        print('*** PROGRAM NAME IS DEFAULT. CHANGE THE PROGRAM NAME!!! ***')
-        exit()
-    ### REMOVE
-    (parsed_options, argvs) = fnGetOptions(fnSetOptions())
-    if fnInit(parsed_options):
-        LOGGER.info('Start %s...' % (PROG_NAME))
-        if fnGetConfig(parsed_options.o_sConfigFilePath):
-            LOGGER.info('Config file("%s")' % (parsed_options.o_sConfigFilePath))
-            fnMain(parsed_options, argvs)
-        LOGGER.info('Terminate %s...' % (PROG_NAME))
+  ### REMOVE
+  if PROG_NAME == '<PROGRAM_NAME>':
+    print('*** PROGRAM NAME IS DEFAULT. CHANGE THE PROGRAM NAME!!! ***')
+    exit()
+  ### REMOVE
+  (parsed_options, argvs) = fnGetOptions(fnSetOptions())
+  if fnInit(parsed_options):
+    LOGGER.info('Start %s...' % (PROG_NAME))
+    if fnGetConfig(parsed_options.o_sConfigFilePath):
+      LOGGER.info('Config file("%s")' % (parsed_options.o_sConfigFilePath))
+      fnMain(parsed_options, argvs)
+    LOGGER.info('Terminate %s...' % (PROG_NAME))
--- python/default_daemon.py
+++ python/default_daemon.py
@@ -32,215 +32,215 @@ CONFIG = {}
 
 #=============================== Daemon Functions ===============================#
 def fnDo():
-    global LOGGER
+  global LOGGER
 
-    try:
-        time.sleep(3)
-        LOGGER.info('running...')
-    except SystemExit:
-        LOGGER.info(' * Signal Exit...')
-    except:
-        LOGGER.error(' *** Daemon error!')
-        LOGGER.debug(traceback.format_exc())
-    finally:
-        LOGGER.info(' * Terminate function...')
-    return True
+  try:
+    time.sleep(3)
+    LOGGER.info('running...')
+  except SystemExit:
+    LOGGER.info(' * Signal Exit...')
+  except:
+    LOGGER.error(' *** Daemon error!')
+    LOGGER.debug(traceback.format_exc())
+  finally:
+    LOGGER.info(' * Terminate function...')
+  return True
 
 def fnStartDaemon():
-    global LOGGER
-    global LOG_FILE_NO
-    global PID_FILENAME
+  global LOGGER
+  global LOG_FILE_NO
+  global PID_FILENAME
 
-    try:
-        context = daemon.DaemonContext(
-            working_directory='./',
-            umask=0o002,
-            pidfile=pidfile.TimeoutPIDLockFile(PID_FILENAME)
-        )
-        context.files_preserve = LOG_FILE_NO
-        with context:
-            LOGGER.info(' * START Daemon!')
-            fnDo()
-    except:
-        LOGGER.error(' *** Error Start Daemon!!!')
-        LOGGER.debug(traceback.format_exc())
+  try:
+    context = daemon.DaemonContext(
+      working_directory='./',
+      umask=0o002,
+      pidfile=pidfile.TimeoutPIDLockFile(PID_FILENAME)
+    )
+    context.files_preserve = LOG_FILE_NO
+    with context:
+      LOGGER.info(' * START Daemon!')
+      fnDo()
+  except:
+    LOGGER.error(' *** Error Start Daemon!!!')
+    LOGGER.debug(traceback.format_exc())
 
-    return True
+  return True
 
 def fnStopDaemon():
-    global LOGGER
-    global PID_FILENAME
+  global LOGGER
+  global PID_FILENAME
 
-    try:
-        LOGGER.info(' * STOP Daemon!')
-        if os.path.isfile(PID_FILENAME):
-            pid = None
-            f = open(PID_FILENAME, 'r')
+  try:
+    LOGGER.info(' * STOP Daemon!')
+    if os.path.isfile(PID_FILENAME):
+      pid = None
+      f = open(PID_FILENAME, 'r')
 
-            for line in f:
-                pid = line.strip()
-            
-            f.close()
+      for line in f:
+        pid = line.strip()
+      
+      f.close()
 
-            if pid is not None:
-                LOGGER.info(' * Kill Daemon PID(%s)' % (pid))
-                os.system(('kill %s' % (pid)))
-            else:
-                LOGGER.info(' * Daemon PID is None')
-        else:
-            LOGGER.info(' * Daemon is not running...')
-    except Exception as e:
-        LOGGER.error(' *** Error Stop Daemon!!!')
-        LOGGER.error(e)
-        LOGGER.debug(traceback.format_exc())
+      if pid is not None:
+        LOGGER.info(' * Kill Daemon PID(%s)' % (pid))
+        os.system(('kill %s' % (pid)))
+      else:
+        LOGGER.info(' * Daemon PID is None')
+    else:
+      LOGGER.info(' * Daemon is not running...')
+  except Exception as e:
+    LOGGER.error(' *** Error Stop Daemon!!!')
+    LOGGER.error(e)
+    LOGGER.debug(traceback.format_exc())
 
-    return True
+  return True
 
 def fnStatusDaemon():
-    global LOGGER
-    global PID_FILENAME
+  global LOGGER
+  global PID_FILENAME
 
-    try:
-        LOGGER.info(' * CHECK STATUS Daemon!')
-        if os.path.isfile(PID_FILENAME):
-            pid = None
-            f = open(PID_FILENAME, 'r')
+  try:
+    LOGGER.info(' * CHECK STATUS Daemon!')
+    if os.path.isfile(PID_FILENAME):
+      pid = None
+      f = open(PID_FILENAME, 'r')
 
-            for line in f:
-                pid = line.strip()
-            
-            f.close()
+      for line in f:
+        pid = line.strip()
+      
+      f.close()
 
-            if pid is not None:
-                LOGGER.info(' * Daemon PID(%s)' % (pid))
-            else:
-                LOGGER.info(' * Daemon PID is None')
-        else:
-            LOGGER.info(' * Daemon is not running...')
-    except Exception as e:
-        LOGGER.error(' *** Error Status Daemon!!!')
-        LOGGER.error(e)
-        LOGGER.debug(traceback.format_exc())
+      if pid is not None:
+        LOGGER.info(' * Daemon PID(%s)' % (pid))
+      else:
+        LOGGER.info(' * Daemon PID is None')
+    else:
+      LOGGER.info(' * Daemon is not running...')
+  except Exception as e:
+    LOGGER.error(' *** Error Status Daemon!!!')
+    LOGGER.error(e)
+    LOGGER.debug(traceback.format_exc())
 
-    return True
+  return True
 
 #=============================== Main Functions ===============================#
 def fnMain(argOptions, argArgs):
-    global LOGGER
+  global LOGGER
 
-    cmd = argArgs[0].lower()
+  cmd = argArgs[0].lower()
 
-    try:
-        if cmd == 'start':
-            if fnGetConfig(parsed_options.o_sConfigFilePath):
-                LOGGER.info('Config file("%s")' % (parsed_options.o_sConfigFilePath))
-                fnStartDaemon()
-        elif cmd == 'stop':
-            fnStopDaemon()
-        elif cmd == 'restart':
-            fnStopDaemon()
-            fnStartDaemon()
-        elif cmd == 'status':
-            fnStatusDaemon()
+  try:
+    if cmd == 'start':
+      if fnGetConfig(parsed_options.o_sConfigFilePath):
+        LOGGER.info('Config file("%s")' % (parsed_options.o_sConfigFilePath))
+        fnStartDaemon()
+    elif cmd == 'stop':
+      fnStopDaemon()
+    elif cmd == 'restart':
+      fnStopDaemon()
+      fnStartDaemon()
+    elif cmd == 'status':
+      fnStatusDaemon()
 
-        return True
-    except:
-        raise
+    return True
+  except:
+      raise
 
 #=============================== Config & Init Function ===============================#
 def fnGetConfig(argConfigFilePath):
-    global LOGGER
-    global CONFIG
+  global LOGGER
+  global CONFIG
 
-    try:
-        if os.path.isfile(argConfigFilePath):
-            CONFIG = json.loads(open(argConfigFilePath, encoding='UTF8').read())
-            LOGGER.info(' * Read config data')
-            return True
-        else:
-            LOGGER.error(' * Config file not found.')
-    except:
-        LOGGER.error(' *** Error read config file.')
-        LOGGER.debug(traceback.format_exc())
-    
-    return False
+  try:
+    if os.path.isfile(argConfigFilePath):
+      CONFIG = json.loads(open(argConfigFilePath, encoding='UTF8').read())
+      LOGGER.info(' * Read config data')
+      return True
+    else:
+      LOGGER.error(' * Config file not found.')
+  except:
+    LOGGER.error(' *** Error read config file.')
+    LOGGER.debug(traceback.format_exc())
+  
+  return False
 
 def fnInit(argOptions):
-    global PROG_NAME
-    global LOGGER
-    global LOG_DIR
-    global LOG_FILENAME
-    global LOG_FILE_NO
+  global PROG_NAME
+  global LOGGER
+  global LOG_DIR
+  global LOG_FILENAME
+  global LOG_FILE_NO
 
-    if os.path.isdir(os.path.abspath(LOG_DIR)) is False:
-        os.mkdir(os.path.abspath(LOG_DIR))
+  if os.path.isdir(os.path.abspath(LOG_DIR)) is False:
+    os.mkdir(os.path.abspath(LOG_DIR))
 
-    LOGGER = logging.getLogger(PROG_NAME.replace(' ', ''))
+  LOGGER = logging.getLogger(PROG_NAME.replace(' ', ''))
 
-    if argOptions.o_bVerbose is True:
-        LOGGER.setLevel(logging.DEBUG)
-    else:
-        LOGGER.setLevel(logging.INFO)
+  if argOptions.o_bVerbose is True:
+    LOGGER.setLevel(logging.DEBUG)
+  else:
+    LOGGER.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('[%(levelname)s] - %(filename)s:%(lineno)s\t- %(asctime)s - %(message)s')
-    
-    file_handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when='midnight', backupCount=7, encoding='UTF-8')
-    file_handler.suffix = '%Y%m%d'
-    file_handler.setFormatter(formatter)
+  formatter = logging.Formatter('[%(levelname)s] - %(filename)s:%(lineno)s\t- %(asctime)s - %(message)s')
+  
+  file_handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when='midnight', backupCount=7, encoding='UTF-8')
+  file_handler.suffix = '%Y%m%d'
+  file_handler.setFormatter(formatter)
 
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(formatter)
+  stream_handler = logging.StreamHandler()
+  stream_handler.setFormatter(formatter)
 
-    LOGGER.addHandler(file_handler)
-    LOGGER.addHandler(stream_handler)
+  LOGGER.addHandler(file_handler)
+  LOGGER.addHandler(stream_handler)
 
-    LOG_FILE_NO = [ file_handler.stream.fileno() ]
+  LOG_FILE_NO = [ file_handler.stream.fileno() ]
 
-    return True
+  return True
 
 #=============================== OptionParser Functions ===============================#
 def fnSetOptions():
-    global PROG_VER
+  global PROG_VER
 
-    parser = None
+  parser = None
 
-    # Ref. https://docs.python.org/2/library/optparse.html#optparse-reference-guide
-    options = [
-        { 'Param': ('-c', '--config'), 'action': 'store', 'type': 'string', 'dest': 'o_sConfigFilePath', 'default': 'config.conf', 'metavar': '<Config file path>', 'help': 'Set config file path.\t\tdefault) config.conf (contents type is JSON)' },
-        { 'Param': ('-v', '--verbose'), 'action': 'store_true', 'dest': 'o_bVerbose', 'default': False, 'metavar': '<Verbose Mode>', 'help': 'Set verbose mode.\t\tdefault) False' },
-        { 'Param': ('-t', '--true'), 'action': 'store_true', 'dest': 'o_bTrue', 'default': False, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) False' },
-        { 'Param': ('-f', '--false'), 'action': 'store_false', 'dest': 'o_bFalse', 'default': True, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) True' },
-        { 'Param': ('-s', '--string'), 'action': 'store', 'type': 'string', 'dest': 'o_sString', 'metavar': '<String>', 'help': 'Set string.' },
-        { 'Param': ('-i', '--int'), 'action': 'store', 'type': 'int', 'dest': 'o_iInt', 'metavar': '<Int>', 'help': 'Set int.' },
-    ]
-    usage = '%prog [options] (start|stop|status|restart)\n\tex) %prog start'
+  # Ref. https://docs.python.org/2/library/optparse.html#optparse-reference-guide
+  options = [
+    { 'Param': ('-c', '--config'), 'action': 'store', 'type': 'string', 'dest': 'o_sConfigFilePath', 'default': 'config.conf', 'metavar': '<Config file path>', 'help': 'Set config file path.\t\tdefault) config.conf (contents type is JSON)' },
+    { 'Param': ('-v', '--verbose'), 'action': 'store_true', 'dest': 'o_bVerbose', 'default': False, 'metavar': '<Verbose Mode>', 'help': 'Set verbose mode.\t\tdefault) False' },
+    { 'Param': ('-t', '--true'), 'action': 'store_true', 'dest': 'o_bTrue', 'default': False, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) False' },
+    { 'Param': ('-f', '--false'), 'action': 'store_false', 'dest': 'o_bFalse', 'default': True, 'metavar': '<Bool>', 'help': 'Set bool.\t\tdefault) True' },
+    { 'Param': ('-s', '--string'), 'action': 'store', 'type': 'string', 'dest': 'o_sString', 'metavar': '<String>', 'help': 'Set string.' },
+    { 'Param': ('-i', '--int'), 'action': 'store', 'type': 'int', 'dest': 'o_iInt', 'metavar': '<Int>', 'help': 'Set int.' },
+  ]
+  usage = '%prog [options] (start|stop|status|restart)\n\tex) %prog start'
 
-    parser = OptionParser(usage = usage, version = '%prog ' + PROG_VER)
+  parser = OptionParser(usage = usage, version = '%prog ' + PROG_VER)
 
-    for option in options:
-        param = option['Param']
-        del option['Param']
-        parser.add_option(*param, **option)
+  for option in options:
+    param = option['Param']
+    del option['Param']
+    parser.add_option(*param, **option)
 
-    return parser
+  return parser
 
 def fnGetOptions(argParser):
-    if len(sys.argv) == 1:
-        return argParser.parse_args(['--help'])
+  if len(sys.argv) == 1:
+    return argParser.parse_args(['--help'])
 
-    if len(argParser.parse_args()[1]) == 0:
-        return argParser.parse_args(['--help'])
+  if len(argParser.parse_args()[1]) == 0:
+    return argParser.parse_args(['--help'])
 
-    return argParser.parse_args()
+  return argParser.parse_args()
 
 if __name__ == '__main__':
-    ### REMOVE
-    if PROG_NAME == '<PROGRAM_NAME>':
-        print('*** PROGRAM NAME IS DEFAULT. CHANGE THE PROGRAM NAME!!! ***')
-        exit()
-    ### REMOVE
-    (parsed_options, argvs) = fnGetOptions(fnSetOptions())
-    if fnInit(parsed_options):
-        LOGGER.info('Start %s...' % (PROG_NAME))
-        fnMain(parsed_options, argvs)
-        LOGGER.info('Terminate %s...' % (PROG_NAME))
+  ### REMOVE
+  if PROG_NAME == '<PROGRAM_NAME>':
+    print('*** PROGRAM NAME IS DEFAULT. CHANGE THE PROGRAM NAME!!! ***')
+    exit()
+  ### REMOVE
+  (parsed_options, argvs) = fnGetOptions(fnSetOptions())
+  if fnInit(parsed_options):
+    LOGGER.info('Start %s...' % (PROG_NAME))
+    fnMain(parsed_options, argvs)
+    LOGGER.info('Terminate %s...' % (PROG_NAME))
